da2dd0a044bc3abbd97328a4ead92367789a9cca
IT4211 <rhdqor100@live.co.kr>
2017-01-03 11:32:56 +0900

    Update regscan.py - BaseYaraScanner
    Add Yara scan code

--- regscan.py
+++ regscan.py
@@ -8,21 +8,68 @@ import volatility.plugins.registry.hivelist as hivelist
 import volatility.obj as obj
 import volatility.plugins.common as common # 플러그인 커맨드 처리, window
 import volatility.utils as utils
+import volatility.constants as constants
 import volatility.win32.tasks as tasks
 import volatility.debug as debug
 
+try:
+    import yara
+    has_yara = True
+except ImportError:
+    has_yara = False
+
 #TODO--------------------+
-# Registry value parsing |
+# Registry value parsing
 #------------------------+
 
-
+# 레지스트리의 value들을 전부 가져와서 list에 저장한 후,
+# RegYaraScanner로 던지면,
+# 실행 코드 탐색(...)
 
 #TODO--------------------+
-# yara scan              |
+# yara scan
 #------------------------+
 
+class BaseYaraScanner(object):
 
+    overlap = 1024
 
+    def __init__(self, address_space = None, rules = None ):
+        self.rules = rules
+        self.address_space = address_space
+
+    def scan(self, offset, maxlen):
+        # Start scanning from offset until maxlen:
+        i = offset
+
+        if isinstance(self.rules, list):
+            rules = self.rules
+        else:
+            rules = [self.rules]
+
+        while i < offset + maxlen:
+            # Read some data and match it.
+            to_read = min(constants.SCAN_BLOCKSIZE + self.overlap, offset + maxlen -i)
+            data = self.address_space.zread(i, to_read)
+            if data:
+                for rule in rules:
+                    for match in rule.match(data = data):
+                        for moffset, _name, _value in match.strings:
+                            if moffset < constants.SCAN_BLOCKSIZE:
+                                yield match, moffset + i
+
+            i += constants.SCAN_BLOCKSIZE
+
+class RegYaraScanner(BaseYaraScanner):
+
+    def __init__(self):
+        BaseYaraScanner.__init__()
+
+    def scan(self):
+        #registry yara scan
+        #for value, self.address_space in ~
+            #for match in BaseYaraScanner.scan(self, ~):
+                #yield match
 #----------------------+
 # RegScan Plugin class |
 #----------------------+
@@ -50,3 +97,4 @@ class RegScan(common.AbstractWindowsCommand):
 
 
     #def render_text(self, outfd, data):
+        # 찾은 레지스트리 key, value, (disassembly?)

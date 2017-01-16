5b8b0099a62908ada37711fd95d7346332278378
IT4211 <rhdqor100@live.co.kr>
2017-01-16 21:09:51 +0900

    Modified regscan.py
    오류가 현재 많고 하이브 리스트 출력하는 부분에서 None 타입이라고 반환됨. 확인해봐야 함.

--- README.md
+++ README.md
@@ -16,3 +16,9 @@ Scan for specific registry keys
 ```sh
 $ python vol.py -f infected.vmem --profile=Win7SP1x86 regscan -k %REGKEY%
 ```
+
+#### reference
+
+- http://www.dailysecu.com/news/articleView.html?idxno=5128
+- http://moyix.blogspot.kr/2008/02/enumerating-registry-hives.html
+-
--- regscan.py
+++ regscan.py
@@ -1,14 +1,16 @@
 #-*- coding: utf-8 -*-
-# Author : SeonHo Lee (IT4211)
-# Email : rhdqor100@live.co.kr
-# Description : Volatility plug-in that scans the registry from the memory dump to find malware.
-
+"""
+@Author : SeonHo Lee (IT4211)
+@E-mail : rhdqor100@live.co.kr
+@Description : Volatility plug-in that scans the registry from the memory dump to find malware.
+"""
 import volatility.plugins.registry.registryapi as registryapi # 레지스트리 API 사용, 메모리 덤프내 레지스트리 처리
-import volatility.plugins.registry.hivelist as hivelist
+import volatility.plugins.registry.hivelist as hl
 import volatility.obj as obj
 import volatility.plugins.common as common # 플러그인 커맨드 처리, window
 import volatility.utils as utils
 import volatility.constants as constants
+import volatility.win32.hive as hivemod
 import volatility.win32.tasks as tasks # 이건 프로세스 목록 얻어오는 용도?여서 안쓸 듯.
 import volatility.debug as debug
 
@@ -32,7 +34,9 @@ except ImportError:
 # RegYaraScanner로 던지면,
 # 실행 코드 탐색(...)
 
-class RegList(hivelist.HiveList):
+# Hivelist를 플러그인 메인으로 부터 전달 받음
+# printkey 플러그인은 특정 레지 키의 서브키, 밸류, 데이터를 보여주는 명령어
+# 모든 hives로 부터 검색을 하여 찾아진 key information을 출력
 
 
 
@@ -76,35 +80,63 @@ class RegYaraScanner(BaseYaraScanner):
         BaseYaraScanner.__init__()
 
     def scan(self):
+        pass
         #registry yara scan
         #for value, self.address_space in ~
             #for match in BaseYaraScanner.scan(self, ~):
                 #yield match
+
 #----------------------+
 # RegScan Plugin class |
 #----------------------+
 
 class RegScan(common.AbstractWindowsCommand):
+    Malware_preferred_registry = []
 
     def __init__(self, config, *args, **kwargs):
         common.AbstractWindowsCommand.__init__(self, config, *args, **kwargs)
+
         config.add_option('HIVE-OFFSET', short_option = 'o',
                           help = 'Hive offset (virtual)', type ='int')
         config.add_option('KEY', short_option= 'K',
                           help= 'Registry Key', type='str')
 
     def calculate(self):
+        hivelists = []
         # 메모리 이미지 물리 주소 영역 호출, 물리 주소 영역의 base 주소 return
         addr_space = utils.load_as(self._config, astype='physical')
-        #self.regapi = registryapi.RegistryApi(self._config)
+        regapi = registryapi.RegistryApi(self._config)
 
-        # 하이브 오프셋 지정 여부
-        if not self._config.HIVE_OFFSET:
-            hive_offsets = [h.obj_offset for h in hivelist.HiveList.calculate(self)]
-        else:
-            hive_offsets = [self._config.HIVE_OFFSET]
+        # hive 전체 리스트를 얻어 온다. 어떻게?
+        regapi.populate_offsets()
+        print "[debug] debug message"
+        tmp = regapi.print_offsets()
+        print "[debug:type]", type(tmp)
+        tmp = str(tmp).split('\n')
+        print "[debug:list]", tmp
+        for hive_offset in tmp:
+            if hive_offset == None:
+                continue
+            else:
+                hive_offset = hive_offset.split(' ')[-1]
+                hivelists.append(hive_offset)
+
+        print "[debug]",hivelists
+
+        # 각각의 하이브에 대해서 스캔을 수행한다.
+        # 하이브에서 가장 최근에 수정된 레지스트리 키를 얻어 온다.
+
+        for hive in hivelists:
+            for regtime, regname, keyname, in regapi.reg_get_last_modified(hive, count = 25):
+                # 여기서 얻어온 키네임을 통해서 value 값 얻어오기!
+                val = regapi.reg_get_value(self, hive, keyname, keyname.split("\\")[-1])
+                yield val
 
 
 
-    #def render_text(self, outfd, data):
+
+    # data는 calculate에서 리턴된 값의 인자. generater 타입으로 리턴하기 때문에 for문이 필수
+    def render_text(self, outfd, data):
         # 찾은 레지스트리 key, value, (disassembly?)
+        for proc in data:
+            outfd.write("{0:s} \n".format(proc))
